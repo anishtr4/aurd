df35300cfda9d9cd7a2e1e58b983dfeb44d8fd49
travis <travis@browser.html>
2015-08-04 08:44:36 -0700

    CCC

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
 
 
 var app = {
-    macAddress: "AA:BB:CC:DD:EE:FF",  // get your mac address from bluetoothSerial.list
+    macAddress: "20:15:05:05:74:05",  // get your mac address from bluetoothSerial.list
     chars: "",
 
 /*
